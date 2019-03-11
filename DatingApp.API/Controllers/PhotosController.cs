2f6f81ddddd6f9eaa6ee2c7ee5f757d3082c4e39
Alicja Gadecka <alicja.gadecka@issp.pl>
2019-03-11 17:15:50 +0100

    Photo controller p1

---
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using DatingApp.API.Data;
+using DatingApp.API.DTOs;
+using DatingApp.API.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API.Controllers
+{
+
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+private readonly IDatingRepository _repo;
+private readonly IOptions<CloudinarySettings> _cloudinaryConfig; 
+private readonly IMapper _mapper;
+        private Cloudinary _cloudinary;
+
+        public PhotosController(IDatingRepository repo, IMapper mapper, 
+        IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            this._cloudinaryConfig = cloudinaryConfig;
+            this._repo = repo;
+            this._mapper = mapper; 
+
+            var acc = new Account(
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecrets
+            ); 
+
+            _cloudinary = new Cloudinary(acc); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId,
+         PhotoForCreationDto photoForCreationDto) 
+        {
+            
+        }
+
+    }
+
+
+    
+}
